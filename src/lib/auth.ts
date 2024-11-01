f546dcf0c992b3db335234a9acc371cc1e679b1a
Sam <samllama55@gmail.com>
2024-11-01 06:51:12 +0100

    Attempt fix bcrypt issue deno.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { JWT_SECRET, NODE_ENV } from '$env/static/private';
 import type { RequestEvent } from '@sveltejs/kit';
-import * as bcrypt from 'bcrypt';
+import * as bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { z } from 'zod';
 import { type User } from './server/db/schema';
@@ -66,9 +66,9 @@ export function authorizeAdmin(
 }
 
 export function saltAndHashPassword(password: string) {
-	return bcrypt.hash(password, 13);
+	return bcryptjs.hash(password, 13);
 }
 
 export function verifyPassword(password: string, hash: User['password']) {
-	return bcrypt.compare(password, hash);
+	return bcryptjs.compare(password, hash);
 }
