3d1ef17b47dad16e61dc353979af57cbb37c6218
Sam Peterson <samllama55@gmail.com>
2024-12-27 15:09:43 +0100

    CHANGE: Links for admin landing page.

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,5 +1,6 @@
-<script lang="ts">
-  //
-</script>
-
-TODO: ADD CONTENT HERE
+<div class="grid grid-cols-2 gap-2">
+  <a class="btn preset-tonal" href="/admin/links">Links</a>
+  <a class="btn preset-tonal" href="/admin/posts">Posts</a>
+  <!-- <a href="/admin/tags">Tags</a>
+  <a href="/admin/users">Users</a> -->
+</div>
