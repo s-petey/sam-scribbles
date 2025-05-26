edd816e8352026b736f88c0b165de4e1d0e9f1cd
Sam <samllama55@gmail.com>
2025-05-26 17:42:05 +0200

    CHANGE: Split hooks into separate handlers.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,18 +5,28 @@ import { svelteKitHandler } from 'better-auth/svelte-kit';
 import { core } from '$lib/siteLinks';
 import { setServerCookies } from '$lib/auth.server';
 import { route } from '$lib/ROUTES';
+import { sequence } from '@sveltejs/kit/hooks';
+import { logger } from '$lib/logger';
 
 const ONE_YEAR = 60 * 60 * 24 * 365;
 
 const DEFAULT_THEME: Theme = 'fennec';
 const DEFAULT_MODE: ThemeMode = 'dark';
 
-export const handle = (async ({ event, resolve }) => {
+const handleAnnoyances: Handle = async ({ event, resolve }) => {
   // Suppress well-known Chrome DevTools requests
   if (event.url.pathname.startsWith('/.well-known/appspecific/com.chrome.devtools')) {
     return new Response(null, { status: 204 }); // Return empty response with 204 No Content
   }
 
+  return await resolve(event);
+};
+
+const handleAuth: Handle = async ({ event, resolve }) => {
+  return await svelteKitHandler({ auth, event, resolve });
+};
+
+export const handleLocals: Handle = async ({ event, resolve }) => {
   let theme: Theme | null = null;
   let themeMode: ThemeMode | null = null;
 
@@ -52,37 +62,12 @@ export const handle = (async ({ event, resolve }) => {
     });
   }
 
-  // TODO: Handle auth route re-routing / session stuff...
-  // https://svelte.dev/docs/kit/hooks#Server-hooks-handle
-  // Add this user to the `locals` value.
-  // Ex: https://github.com/aakash14goplani/SvelteKitAuth/blob/main/src/hooks.server.ts
-
   const session = await auth.api.getSession({
     headers: event.request.headers,
     query: {
       disableRefresh: true,
     },
   });
-
-  if (event.url.pathname.startsWith('/admin') && session?.user?.role !== 'admin') {
-    if (session !== null && session.session.expiresAt.valueOf() < Date.now()) {
-      console.log('Session expired, logging out...');
-      // TODO: Warn the user they need to login again (have a message?)
-      // Or have a way to extend the session when it is nearing expiry if
-      // it is active?
-      const response = await auth.api.signOut({
-        returnHeaders: true,
-        headers: event.request.headers,
-      });
-      setServerCookies(response.headers, event);
-
-      // TODO: Add this route to the siteLinks once exposed more clearly...
-      redirect(302, route('/login'));
-    }
-
-    console.log('User is not an admin, redirecting to home...');
-    redirect(303, core.Home.href);
-  }
 
   if (session) {
     event.locals.session = {
@@ -99,35 +84,71 @@ export const handle = (async ({ event, resolve }) => {
   event.locals.theme = { theme: theme ?? DEFAULT_THEME, mode: themeMode ?? DEFAULT_MODE };
 
   if (theme !== null || themeMode !== null) {
-    return svelteKitHandler({
-      event,
-      resolve: async () =>
-        await resolve(event, {
-          transformPageChunk: ({ html }) => {
-            if (isValidMode(cookieThemeMode) && isValidTheme(cookieTheme)) {
-              return html
-                .replace('data-theme="fennec"', `data-theme="${cookieTheme}"`)
-                .replace('class="dark"', `class="${cookieThemeMode === 'dark' ? 'dark' : ''}"`);
-            }
+    return await resolve(event, {
+      transformPageChunk: ({ html }) => {
+        if (isValidMode(cookieThemeMode) && isValidTheme(cookieTheme)) {
+          return html
+            .replace('data-theme="fennec"', `data-theme="${cookieTheme}"`)
+            .replace('class="dark"', `class="${cookieThemeMode === 'dark' ? 'dark' : ''}"`);
+        }
 
-            if (isValidTheme(cookieTheme)) {
-              return html.replace('data-theme="fennec"', `data-theme="${cookieTheme}"`);
-            }
+        if (isValidTheme(cookieTheme)) {
+          return html.replace('data-theme="fennec"', `data-theme="${cookieTheme}"`);
+        }
 
-            if (isValidMode(cookieThemeMode)) {
-              return html.replace(
-                'class="dark"',
-                `class="${cookieThemeMode === 'dark' ? 'dark' : ''}"`,
-              );
-            }
+        if (isValidMode(cookieThemeMode)) {
+          return html.replace(
+            'class="dark"',
+            `class="${cookieThemeMode === 'dark' ? 'dark' : ''}"`,
+          );
+        }
 
-            return html;
-          },
-        }),
-
-      auth,
+        return html;
+      },
     });
   }
 
-  return await svelteKitHandler({ auth, event, resolve });
-}) satisfies Handle;
+  return await resolve(event);
+};
+
+const handleRouting: Handle = async ({ event, resolve }) => {
+  const { session } = event.locals;
+
+  logger.debug({ msg: `Handling routing for: ${event.url.pathname}`, session });
+
+  if (event.url.pathname.startsWith('/admin') && session?.user?.role !== 'admin') {
+    if (session?.session !== null && session.session.expiresAt.valueOf() < Date.now()) {
+      logger.info('Session expired, logging out...');
+      // TODO: Warn the user they need to login again (have a message?)
+      // Or have a way to extend the session when it is nearing expiry if
+      // it is active?
+      const response = await auth.api.signOut({
+        returnHeaders: true,
+        headers: event.request.headers,
+      });
+      setServerCookies(response.headers, event);
+
+      // TODO: Add this route to the siteLinks once exposed more clearly...
+      redirect(302, route('/login'));
+    }
+
+    logger.info('User is not an admin, redirecting to home...');
+    redirect(303, core.Home.href);
+  }
+
+  // TODO: Handle auth route re-routing / session stuff...
+  // https://svelte.dev/docs/kit/hooks#Server-hooks-handle
+  // Add this user to the `locals` value.
+  // Ex: https://github.com/aakash14goplani/SvelteKitAuth/blob/main/src/hooks.server.ts
+
+  return await resolve(event);
+};
+
+export const handle: Handle = sequence(
+  handleAnnoyances,
+  // Ensure auth comes before locals so endpoints are set up.
+  handleAuth,
+  // Ensure locals are before routing so they are available in the routing handler.
+  handleLocals,
+  handleRouting,
+);
