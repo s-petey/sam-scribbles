b47456157b74e7da7b0f5ff9c0c656de7cc10344
Sam Peterson <samllama55@gmail.com>
2025-05-31 10:29:53 +0200

    CHANGE: Post rendering for easier readability. CHANGE: If post is public restriction and banner for admins.

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  // import { route } from '$lib/ROUTES.js';
   import { DateTime } from 'luxon';
 
   let { data } = $props();
@@ -8,7 +7,7 @@
   // however in this case I won't care
   const {
     Content,
-    meta: { isPrivate, date, title, reading_time, tags },
+    meta: { isPrivate, date, title, reading_time, tags, updated },
   } = data;
 
   const dateTime = DateTime.fromJSDate(date);
@@ -19,43 +18,16 @@
   const dateIso = dateTime.toISO() ?? '';
 </script>
 
-<!-- TODO: Heading...
 <svelte:head>
-  <title>Sam's Scribbles - personal website</title>
-  <meta name="description" content="My blog and link sharing website." />
-</svelte:head> -->
+  <title>{title}</title>
+  <meta name="description" content={title} />
+</svelte:head>
 
-<article class="bg-surface-800-200 px-4">
-  <h1 class="mb-1 text-5xl font-black">{title}</h1>
-  <div class="mt-4 mb-3 uppercase">
-    <div class="flex justify-center gap-1">
-      <time datetime={dateIso}>
-        {dateTime.toLocaleString()}
-      </time>
-      &bull;
-      <span>{reading_time.text}</span>
-    </div>
-    <div class="flex justify-center gap-2">
-      <!-- TODO: Implement this page -->
-      {#each tags as tag (tag)}
-        <a href={`/tags/${tag}`}>
-          <!-- <a href={route('/tags/[tag]', { tag: tag.toLowerCase() })}> -->
-          <span
-            class="badge preset-filled-surface-500 hover:bg-surface-contrast-500 hover:text-surface-500 shadow-md transition"
-          >
-            {tag}
-          </span>
-        </a>
-      {/each}
-      {#if dateTime.diffNow('days').days < 31}
-        <span
-          class="text-primary-content badge preset-filled-success-500 hover:bg-success-contrast-500 hover:text-success-500 cursor-pointer font-bold shadow-md transition"
-        >
-          NEW
-        </span>
-      {/if}
-    </div>
-  </div>
+<!-- TODO: Add table of contents per post, load this in the `page.ts`. -->
+
+<article
+  class="dark:bg-surface-400 bg-surface-200 border-surface-800-200 m-auto w-fit rounded border px-4 py-2"
+>
   <!-- {#if current_visitor_data} -->
   <!-- <span
 			onmouseenter={() => (show_current_visitor_data = true)}
@@ -73,20 +45,52 @@
 		</span> -->
   <!-- {/if} -->
 
-  {#if isPrivate}
-    POST ISN'T PUBLISHED YET!
-    <!-- <IsPrivateBanner /> -->
-  {/if}
-
-  <!-- || updated -->
-  {#if DateTime.fromJSDate(date).diffNow('years').years >= 1}
-    It has been updated!
-    <!-- <UpdatedBanner updated={updated === undefined ? date : updated} {date} /> -->
-  {/if}
-
   <div
-    class="prose prose-lg lg:prose-xl prose-headings:scroll-mt-16 prose-a:text-primary-400 prose-a:transition prose-a:hover:text-secondary-400 mb-10"
+    class="prose dark:prose-invert prose-lg lg:prose-xl prose-headings:scroll-mt-16 prose-a:transition prose-a:hover:text-secondary-400 m-auto mb-10"
   >
+    <h1 class="h1 mb-0!">{title}</h1>
+
+    {#if isPrivate}
+      <p class="bg-tertiary-300 dark:bg-tertiary-500 rounded-lg p-2 text-center">
+        POST ISN'T PUBLIC YET!
+      </p>
+    {/if}
+
+    <div class="mt-4 mb-3 uppercase">
+      <div class="flex justify-center gap-1">
+        <time datetime={dateIso}>
+          {dateTime.toLocaleString()}
+        </time>
+        &bull;
+        <span>{reading_time.text}</span>
+        {#if dateTime.diffNow('days').days < 31}
+          &bull;
+          <span class="text-primary-800 font-bold">NEW</span>
+        {/if}
+      </div>
+
+      <div class="flex justify-center gap-2">
+        {#each tags as tag (tag)}
+          <a class="anchor" href={`/tags/${tag}`}>
+            <!-- TODO: Replace when route is created -->
+            <!-- <a href={route('/tags/[tag]', { tag: tag.toLowerCase() })}> -->
+            <span
+              class="badge preset-filled-surface-500 hover:bg-surface-contrast-500 hover:text-surface-500 shadow-md transition"
+            >
+              {tag}
+            </span>
+          </a>
+        {/each}
+      </div>
+    </div>
+
+    <!-- TODO: Should this fall back to date? date || updated -->
+    {#if updated !== undefined && DateTime.fromJSDate(updated).diffNow('months').years <= 4}
+      <p class="text-tertiary-800-200 flex w-full flex-col items-center font-normal italic">
+        Recently updated!
+      </p>
+    {/if}
+
     <Content />
   </div>
 
@@ -122,6 +126,4 @@
   <!-- <PopularPosts /> -->
 
   <!-- <RelatedPosts {related_posts} /> -->
-
-  <!-- <ButtButt /> -->
 </article>
