f6aa6845d9ccf70f409008bd66ea03b3ad0637b6
Sam Peterson <samllama55@gmail.com>
2025-08-03 13:17:31 +0200

    ADD: Specific tag page with consistent styling for posts and links.

--- src/routes/tags/[slug]/+page.server.ts
+++ src/routes/tags/[slug]/+page.server.ts
@@ -1,5 +1,86 @@
-import type { PageServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { type Link, linksToTags, type Post, postsToTags } from '$lib/server/db/schema';
+import { inArray } from 'drizzle-orm';
 
-export const load = (async ({ params }) => {
-  return { slug: params.slug };
-}) satisfies PageServerLoad;
+type PostSummary = Pick<
+  Post,
+  'slug' | 'title' | 'preview' | 'previewHtml' | 'readingTimeSeconds' | 'createdAt'
+> & {
+  tags: string[];
+};
+
+type LinkSummary = Pick<Link, 'shortId' | 'link' | 'createdAt'> & {
+  tags: string[];
+};
+
+// TODO: Handle pagination
+export const load = async ({ params }) => {
+  const { slug } = params;
+
+  let posts: PostSummary[] = [];
+  let links: LinkSummary[] = [];
+
+  if (typeof slug === 'string' && slug.length > 0) {
+    posts = (
+      await db.query.post.findMany({
+        where: (post, { eq }) =>
+          inArray(
+            post.id,
+            db
+              .select({ id: postsToTags.postId })
+              .from(postsToTags)
+              .where(eq(postsToTags.tag, slug)),
+          ),
+        columns: {
+          createdAt: true,
+          slug: true,
+          title: true,
+          readingTimeSeconds: true,
+          preview: true,
+          previewHtml: true,
+        },
+        with: {
+          tags: {
+            columns: { tag: true },
+          },
+        },
+        limit: 25,
+      })
+    ).map((post) => ({
+      createdAt: post.createdAt,
+      slug: post.slug,
+      title: post.title,
+      readingTimeSeconds: post.readingTimeSeconds,
+      preview: post.preview,
+      previewHtml: post.previewHtml,
+      tags: post.tags.map((t) => t.tag),
+    }));
+
+    links = (
+      await db.query.link.findMany({
+        where: (link, { eq }) =>
+          inArray(
+            link.shortId,
+            db
+              .select({ id: linksToTags.linkId })
+              .from(linksToTags)
+              .where(eq(linksToTags.tag, slug)),
+          ),
+        with: {
+          tags: {
+            columns: { tag: true },
+          },
+        },
+        limit: 25,
+        orderBy: (link, { desc }) => desc(link.createdAt),
+      })
+    ).map((link) => ({
+      shortId: link.shortId,
+      link: link.link,
+      createdAt: link.createdAt,
+      tags: link.tags.map((t) => t.tag),
+    }));
+  }
+
+  return { slug, posts, links };
+};
--- src/routes/tags/[slug]/+page.svelte
+++ src/routes/tags/[slug]/+page.svelte
@@ -1,13 +1,72 @@
 <script lang="ts">
-  import type { PageData } from './$types';
+  import { resolve } from '$app/paths';
+  import LinkWithIcon from '$lib/components/LinkWithIcon.svelte';
 
-  let { data }: { data: PageData } = $props();
+  let { data } = $props();
 </script>
 
-<!-- TODO:  
 <svelte:head>
-  <title>Sam's Scribbles - personal website</title>
-  <meta name="description" content="My blog and link sharing website." />
-</svelte:head> -->
+  <title>Sam's Scribbles - Tag: {data.slug}</title>
+  <meta name="description" content="Posts and links tagged with {data.slug}." />
+</svelte:head>
 
-<p>🚧 WIP 🚧 -- {data.slug} -- please come back later!</p>
+<h1 class="h1 mb-4">
+  Tag: {data.slug}
+</h1>
+
+{#if data.posts.length > 0}
+  <h2 class="h2 mb-2">Posts</h2>
+  <ul class="mb-6">
+    {#each data.posts as post (post.slug)}
+      <li class="mb-4 rounded border p-2">
+        <a href={resolve('/posts/[slug]', { slug: post.slug })} class="text-lg font-bold">
+          {post.title}
+        </a>
+        <div class="text-secondary-500 text-sm">{post.preview}</div>
+
+        <ul class="mt-2 flex flex-wrap gap-2">
+          {#each post.tags as tag (tag)}
+            <li class="chip preset-outlined-surface-500">
+              <a
+                href={resolve('/tags/[slug]', { slug: tag })}
+                class="cursor-pointer"
+                title="Show items by {tag}"
+              >
+                {tag}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <p class="text-gray-500">No posts found for this tag.</p>
+{/if}
+
+<h2 class="h2 mb-2">Links</h2>
+{#if data.links.length > 0}
+  <ul>
+    {#each data.links as link (link.shortId)}
+      <li class="mb-4 rounded border p-2">
+        <LinkWithIcon {link} />
+
+        <ul class="mt-2 flex flex-wrap gap-2">
+          {#each link.tags as tag (tag)}
+            <li class="chip preset-outlined-surface-500">
+              <a
+                href={resolve('/tags/[slug]', { slug: tag })}
+                class="cursor-pointer"
+                title="Show items by {tag}"
+              >
+                {tag}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <p class="text-warning-500">No links found for selected tags.</p>
+{/if}
